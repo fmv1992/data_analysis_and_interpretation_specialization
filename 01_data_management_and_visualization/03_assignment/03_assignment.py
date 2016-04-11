0bf4d8312dca7e26a68e17b79d33c03b75ce7f79
fmv1992 <fmv1992@gmail.com>
2016-04-11 00:19:16 -0300

    coursera data management assignment 03

--- 01_data_management_and_visualization/03_assignment/03_assignment.py
+++ 01_data_management_and_visualization/03_assignment/03_assignment.py
@@ -17,14 +17,23 @@ db = pd.read_csv('03_assignment_nesarc_subset.csv',
 db.rename(columns={'numpers':'n_persons',
                    's1q1g':'years_in_us'}, inplace=True)
 
-# converts years in us to int 
+# converts years in us to int
 db.years_in_us = pd.to_numeric(db.years_in_us, errors='coerce')
 db['n_children'] = db['chld0'] + db['chld1_4'] + db['chld5_12']
 
 # aggregates years in us into 5 years range since there is a 100 long range
 # of values
-group_y_in_us = pd.groupby(db, by='years_in_us')
+range_of_years = np.arange(0, 101, 10)
+db['years_in_us_grouped'] = pd.cut(db.years_in_us, range_of_years,
+                                   include_lowest=True)
 
-# prints 
-for c in ['n_children', 'years_in_us', 'n_persons']:
-    print(c, db[c].value_counts(normalize=True), sep='\n')
+# prints
+for c in ['n_children', 'years_in_us_grouped', 'n_persons']:
+    v_counts = db[c].value_counts(normalize=True, sort=False)
+    print(c, v_counts, sep='\n')
+    v_counts.plot.bar()
+    plt.ylabel('frequency of ' + c)
+    plt.tight_layout()
+    plt.savefig('{0}_plot.png'.format(c), dpi=500)
+    plt.show()
+    plt.close()
