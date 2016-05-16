63523473bdbf9156fbfd5a826f5b4409b5bb789e
fmv1992 <fmv1992@gmail.com>
2016-05-16 00:44:11 -0300

    course 03 assignment 03

---
+++ 03_regression_modeling_in_practice/assignment_03/pandas_utilities.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 07:37:20 2016
+
+@author: e061568
+"""
+import pandas as pd
+import string
+
+universe_of_digits = string.ascii_lowercase + string.digits + '_'
+
+def clean_string(s):
+    """Takes s as str input and makes it a standard lowercase with '_' as
+    separator."""
+    s = str(s).lower().replace(' ', '_')
+    return ''.join(filter(lambda x: x in universe_of_digits, s))
+    
+
+def clean_dataframe(df):
+    """Cleans the dataframe."""
+    df.rename(columns=clean_string, inplace=True)
+    return None
