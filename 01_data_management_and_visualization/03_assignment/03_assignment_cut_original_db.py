7ef51a6b5eac1d9a2dccaeb3824c68da76a8f91c
Felipe M. Vieira <fmv1992@gmail.com>
2018-07-31 08:55:53 -0300

    commented out script 03_assignment_cut_original_db.py

--- 01_data_management_and_visualization/03_assignment/03_assignment_cut_original_db.py
+++ 01_data_management_and_visualization/03_assignment/03_assignment_cut_original_db.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Apr 10 17:54:35 2016
+Subset NESARC data.
+
+This script has been commented out because the subset data is available, but
+not the original one.
 
 Author: Felipe M. Vieira
 """
@@ -8,8 +10,8 @@ import pandas as pd  # version '0.18.0'
 import seaborn  # version '0.7.0'
 import matplotlib.pyplot as plt  # version'1.3.1'
 
-db = pd.read_csv('01_u.s._national_epidemiological_survey_on_alcohol_and'
-                 '_related_conditions_nesarc.csv', low_memory=False)
-db.rename(columns=lambda x: str(x).lower(), inplace=True)
-db[['chld0', 'chld1_4', 'chld5_12', 'numpers', 's1q1g']].to_csv(
-'03_assignment_nesarc_subset.csv')
+# db = pd.read_csv('01_u.s._national_epidemiological_survey_on_alcohol_and'
+#                  '_related_conditions_nesarc.csv', low_memory=False)
+# db.rename(columns=lambda x: str(x).lower(), inplace=True)
+# db[['chld0', 'chld1_4', 'chld5_12', 'numpers', 's1q1g']].to_csv(
+# '03_assignment_nesarc_subset.csv')
